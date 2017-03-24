5ca9dbae8d6915269eb2c00a60ff4283df3c58e0
zhangya <zhangya998@gmail.com>
2017-03-25 01:15:37 +0800

    init

---
+++ dataProcesser.py
@@ -0,0 +1,48 @@
+import scipy.io
+import numpy
+
+def loadExpression(data):
+    mat = scipy.io.loadmat('data/express/matlab.mat');
+    data['express'] = mat['exp_matrix'];
+    data['label'] = numpy.logical_or(mat['S1'][:,3] == 'Stable Disease', mat['S1'][:,3] == 'Clinical Progressive Disease');
+    data['uid'] = mat['S1'][:,1];
+    data['drug'] = mat['S1'][:,2];
+    data['disease'] = mat['S1'][:,0]
+
+
+def loadCNV(data):
+    mat = scipy.io.loadmat('data/CNV/CNV.mat');
+
+    patients = mat['U'][:,1];
+    cnv = mat['M'];
+
+    cnvdata = numpy.zeros([data['uid'].shape[0], cnv.shape[0]])
+    found = False;
+    count = 0.0;
+    for i in range(data['uid'].shape[0]):
+        for j in range(patients.shape[0]):
+            if patients[j][0][0:12] == data['uid'][i][0]:
+                cnvdata[i] = cnv[:,j];
+                found = True;
+                count += 1;
+                break;
+
+        if not found:
+            print("warning: not found %s" % data['uid'][i][0]);
+
+        found = False;
+
+    print("found percentage %f" % (count/data['uid'].shape[0]));
+
+    data['cnv'] = cnvdata;
+
+def main():
+    data = {};
+    loadExpression(data);
+    loadCNV(data);
+
+    print data;
+
+if __name__ == "__main__":
+    main();
+    
---
+++ trAdaboost.py
@@ -0,0 +1,46 @@
+import numpy
+import math
+from sklearn import svm
+
+def trAdaboost(Td, Ts, labeld, labels, S, N):
+    # get the length of all labeled data
+
+    t = Td.shape[0] + Ts.shape[0]
+    n = Td.shape[0]
+
+    # init weight vector
+    wd = numpy.ones([Td.shape[0], 1])
+    ws = numpy.ones([Ts.shape[0], 1])
+    ht = numpy.zeros([N, S.shape[0]])
+
+    beta = 0
+    beta_t = numpy.zeros([1, N])
+    for i in range(N):
+        clf_weights = svm.SVC()
+        clf_weights.fit(numpy.vstack((Td, Ts)), numpy.hstask((labeld, labels)),
+                        sample_weight=numpy.hstask((wd, ws)) / (numpy.sum(ws) + numpy.sum(wd)))
+        hs = clf_weights.predict(Ts)
+        hd = clf_weights.predict(Td)
+        ht[i, :] = clf_weights.predict(S)
+        # TODO: error_t suppost to be less than 1/2
+        error_t = numpy.dot(numpy.abs(hs - labels), ws) / numpy.sum(ws)
+
+        beta_t[1, i] = error_t / (1 - error_t)
+        beta = 1. / (1 + sqrt(2. * ln(float(n) / N)))
+
+        wd = wd * (beta ** abs(hd - labeld))
+        ws = ws * (beta_t[1, i] ** -abs(hs - labels))
+
+    hf = numpy.zeros([1, S.shape[0]])
+    base = numpy.prod(beta_t[1, math.ceil(N / 2) : N] ** -0.5)
+    for i in range(S.shape[0]):
+        # production of this powers
+        posibity = numpy.prod(beta_t[1, math.ceil(N / 2) : N] ** -ht[i, math.ceil(N / 2) : N]))
+
+        if(posibity > base):
+            hf[1, i] = 1
+        else:
+            hf[1, i] = 0
+
+    return hf
+        
