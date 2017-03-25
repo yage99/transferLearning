8b1334714f4d1cd1ab49055ceb010de273838746
zhangya <zhangya998@gmail.com>
2017-03-25 18:48:50 +0800

    This is python

--- dataProcessor.py
+++ dataProcessor.py
@@ -7,10 +7,10 @@ import numpy
 def loadExpression(data):
     """Loads data from specified matlab mat file whose path is set to `data/express/matlab.mat`.
 
-Paramaters:
-------------------------------------------------
-data: dict
-    data will be filled with loaded matrices.
+    Paramaters:
+    ------------------------------------------------
+    data: dict
+        data will be filled with loaded matrices.
     """
     mat = scipy.io.loadmat('data/express/matlab.mat')
     data['express'] = mat['exp_matrix']
@@ -22,38 +22,45 @@ data: dict
 
 
 def loadCNV(data):
-    mat = scipy.io.loadmat('data/CNV/CNV.mat');
+    """Load CNV data from mat file. Path is set to `data/CNV/CNV.mat`.
 
-    patients = mat['U'][:,1];
-    cnv = mat['M'];
+    Paramaters:
+    ------------------------------------------------
+    data: dict
+        This paramater should be initialized by :func:`loadExpression` first, for the patients id is loaded by :func:`loadExpression`.
+"""
+    mat = scipy.io.loadmat('data/CNV/CNV.mat')
+
+    patients = mat['U'][:,1]
+    cnv = mat['M']
 
     cnvdata = numpy.zeros([data['uid'].shape[0], cnv.shape[0]])
-    found = False;
-    count = 0.0;
+    found = False
+    count = 0.0
     for i in range(data['uid'].shape[0]):
         for j in range(patients.shape[0]):
             if patients[j][0][0:12] == data['uid'][i][0]:
-                cnvdata[i] = cnv[:,j];
-                found = True;
-                count += 1;
-                break;
+                cnvdata[i] = cnv[:,j]
+                found = True
+                count += 1
+                break
 
         if not found:
-            print("warning: not found %s" % data['uid'][i][0]);
+            print("warning: not found %s" % data['uid'][i][0])
 
-        found = False;
+        found = False
 
-    print("found percentage %f" % (count/data['uid'].shape[0]));
+    print("found percentage %f" % (count/data['uid'].shape[0]))
 
-    data['cnv'] = cnvdata;
+    data['cnv'] = cnvdata
 
 def main():
-    data = {};
-    loadExpression(data);
-    loadCNV(data);
+    data = {}
+    loadExpression(data)
+    loadCNV(data)
 
-    print data;
+    print data
 
 if __name__ == "__main__":
-    main();
+    main()
     
