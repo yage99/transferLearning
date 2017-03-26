6508bbff85204d25db0b9b393c08961e99f2ecb3
zhangya <zhangya998@gmail.com>
2017-03-27 03:17:07 +0800

    add feature selection logic

--- dataProcessor.py
+++ dataProcessor.py
@@ -72,7 +72,7 @@ def data_filter(data, drug_name, disease_name=None):
     Returned a new dict contains all filtered data.
     """
     filter = data['drug'] == drug_name
-    if disease_name != None:
+    if disease_name is not None:
         filter = numpy.logical_and(filter, data['disease'] == diseaes_name)
 
     filtered_data = {}
@@ -82,52 +82,69 @@ def data_filter(data, drug_name, disease_name=None):
     filtered_data['disease'] = data['disease'][filter]
 
     return filtered_data
-    
+
 
 def test():
+    """Main test function"""
+    
+    source_drug = "Cisplatin"
+    target_drug = "Carboplatin"
+    feature_num = 500
+    disease = None
+
     data = {}
     loadExpression(data)
 
     # normalize gene express based on all data
     expression = data['express']
     expression = expression[:, expression.std(0) != 0]
-    expression = (expression -
-                  numpy.resize(expression.mean(0),
-                               expression.shape)) / numpy.resize(expression.std(0), expression.shape)
+    expression = ((expression - numpy.resize(expression.mean(0),
+                                             expression.shape))
+                  / numpy.resize(expression.std(0), expression.shape))
     expression[numpy.logical_or(expression > 2, expression < -2)] = 0
     data['express'] = preprocessing.MinMaxScaler().fit_transform(expression)
 
     # filter two drugs as source and target data
-    source_data = data_filter(data, 'Cisplatin')
-    target_data = data_filter(data, 'Carboplatin')
+    source_data = data_filter(data, source_drug, disease)
+    target_data = data_filter(data, target_drug, disease)
 
     source_express = source_data['express']
     target_express = target_data['express']
 
     # do feature selction by mrmr
-    if(os.path.isfile('~feature.txt')):
+    feature_file_name = ('~feature_%s_%s_%s_%d.txt'
+                         % (source_drug, target_drug, disease, feature_num))
+    if(os.path.isfile(feature_file_name)):
         print "Feature file found, use existing features"
-        feature_indc = numpy.loadtxt('~feature.txt').astype('int')
+        feature_indc = numpy.loadtxt(feature_file_name).astype('int')
     else:
         print "Feature file not found, feature selection by mrmr"
-        numpy.savetxt("~temp.csv", numpy.ceil(source_express*100), fmt="%.3f", delimiter=',')
-        output = subprocess.check_output(["mrmr_c_src/mrmr", "-i", "temp.csv", "-n", "200", "-s", "4000", "-v", "21000"])
+        numpy.savetxt("~temp.csv", numpy.ceil(source_express*100),
+                      fmt="%.3f", delimiter=',')
+        output = subprocess.check_output(["mrmr_c_src/mrmr", "-i", "~temp.csv",
+                                          "-n", str(feature_num),
+                                          "-s",
+                                          str(source_express.shape[0] + 10),
+                                          "-v",
+                                          str(source_express.shape[1] + 10)])
+        os.remove("~temp.csv")
         # load features from data
-        table = numpy.fromstring(output).reshape(200, 4)
+        table = numpy.fromstring(output, sep='\t').reshape(feature_num, 4)
         feature_indc = table[:, 1].astype('int')
-        numpy.savetxt('~feature.txt', feature_indc)
+        numpy.savetxt(feature_file_name, feature_indc)
         print "Feature selection finished"
 
     source_express = source_express[:, feature_indc]
     target_express = target_express[:, feature_indc]
-    print "Instance number, source: %d, target: %d" % (source_express.shape[0], target_express.shape[0])
+    print ("Instance number, source: %d, target: %d"
+           % (source_express.shape[0], target_express.shape[0]))
 
     source_label = numpy.zeros(source_data['label'].shape)
     source_label[source_data['label']] = 1
     target_label = numpy.zeros(target_data['label'].shape)
     target_label[target_data['label']] = 1
-    
-    kf = KFold(n_splits = 10)
+
+    kf = KFold(n_splits=10)
     predict = numpy.zeros(target_label.shape)
 
     C_range = 2. ** numpy.arange(-2, 4, 0.5)
@@ -135,15 +152,18 @@ def test():
     param_grid = dict(gamma=gamma_range, C=C_range)
     cv = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42)
 
-    best_clf = gridSearchCV(source_express, source_label, param_grid=param_grid, cv=cv)
+    best_clf = gridSearchCV(source_express, source_label,
+                            param_grid=param_grid,
+                            cv=cv)
     return
     for train, test in kf.split(target_label):
         print "training one split"
         predict[test] = trAdaboost(source_express, target_express[train],
-                             source_label, target_label[train],
-                             target_express[test], 10)
-        
-    print "The overall trAdaboost AUC is %f" % metrics.roc_auc_score(target_label, predict)
+                                   source_label, target_label[train],
+                                   target_express[test], 10)
+
+    print ("The overall trAdaboost AUC is %f"
+           % metrics.roc_auc_score(target_label, predict))
 
 
 if __name__ == "__main__":
